f148ab42b5e54957a244150cc67cbbbc1b48c9eb
Peter Till <till.peter@goatvids.ga>
2022-08-24 17:27:29 +0200

    fixed property of undefined

--- dep/index.js
+++ dep/index.js
@@ -2,7 +2,7 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-module.exports = (app) => {
+ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
@@ -95,82 +95,114 @@ module.exports = (app) => {
       body: "Thanks for opening this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.edited", async (context) => {
+  });
+  
+  app.on("pull_request.edited", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for editing this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.labeled", async (context) => {
+  });
+  
+  app.on("pull_request.labeled", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for adding a label!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.unlabeled", async (context) => {
+  });
+  
+  app.on("pull_request.unlabeled", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for removing a label!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.assigned", async (context) => {
+  });
+  
+  app.on("pull_request.assigned", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for assigning this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.unassigned", async (context) => {
+  });
+  
+  app.on("pull_request.unassigned", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for unassigning this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.review_requested", async (context) => {
+  });
+  
+  app.on("pull_request.review_requested", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for requesting a review!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.review_request_removed", async (context) => {
+  });
+  
+  app.on("pull_request.review_request_removed", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for removing a review request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.synchronize", async (context) => {
+  });
+  
+  app.on("pull_request.synchronize", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for updating this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.closed", async (context) => {
+  });
+  
+  app.on("pull_request.closed", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for closing this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.reopened", async (context) => {
+  });
+  
+  app.on("pull_request.reopened", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for reopening this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.merged", async (context) => {
+  });
+  
+  app.on("pull_request.merged", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for merging this pull request!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.head_ref_deleted", async (context) => {
+  });
+  
+  app.on("pull_request.head_ref_deleted", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for deleting the head ref!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.head_ref_restored", async (context) => {
+  });
+  
+  app.on("pull_request.head_ref_restored", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for restoring the head ref!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.head_ref_force_pushed", async (context) => {
+  });
+  
+  app.on("pull_request.head_ref_force_pushed", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for force pushing the head ref!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.review_requested", async (context) => {
+  });
+  
+  app.on("pull_request.review_requested", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for requesting a review!",
     });
     return context.octokit.issues.createComment(issueComment);
-  }).on("pull_request.review_request_removed", async (context) => {
+  });
+  
+  app.on("pull_request.review_request_removed", async (context) => {
     const issueComment = context.issue({
       body: "Thanks for removing a review request!",
     });
